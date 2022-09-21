bcbce4adf28a24e2f3169a4f436f3d51c8689286
efimero <martinperez9713@gmail.com>
2022-09-21 15:01:17 -0300

    update desafio sockets

--- pug/routes/products.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const productsRouter = Router();
-import { get, addProduct } from "../controllers/productsController.js";
-
-productsRouter.get("/", get)
-productsRouter.post("/", addProduct)
-
-export default productsRouter;

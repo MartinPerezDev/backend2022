a88fcc98651c3c8333014780bcf41d83b3187fa4
efimero <martinperez9713@gmail.com>
2022-09-25 22:24:24 -0300

    fix glitch

---
+++ dist/data/cart.js
@@ -0,0 +1,11 @@
+"use strict";
+
+[{
+  "id": 1,
+  "timestamp": "25/09/2022 21:56",
+  "products": []
+}, {
+  "id": 2,
+  "timestamp": "25/09/2022 21:58",
+  "products": []
+}];
---
+++ dist/data/products.js
@@ -0,0 +1,30 @@
+"use strict";
+
+[{
+  "id": 1,
+  "name": "producto1",
+  "description": "this is product 1",
+  "code": "Asx13as",
+  "image": "",
+  "price": 1233,
+  "stock": 23,
+  "timestamp": "25/09/2022 22:01"
+}, {
+  "id": 2,
+  "name": "producto2",
+  "description": "this is product 1",
+  "code": "Asx13as",
+  "image": "",
+  "price": 1233,
+  "stock": 23,
+  "timestamp": "25/09/2022 22:01"
+}, {
+  "id": 3,
+  "name": "producto 3",
+  "description": "this is product 1",
+  "code": "Asx13as",
+  "image": "",
+  "price": 1233,
+  "stock": 23,
+  "timestamp": "25/09/2022 22:02"
+}];
